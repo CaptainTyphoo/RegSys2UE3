e34e487dea6064c1078c2346d74ae2b420159351
tsirpz <e1026833@mail.student.tuwien.ac.at>
2016-01-04 20:18:09 +0100

    bis 5.3

---
+++ Riementrieb/parameter.m
@@ -0,0 +1,14 @@
+
+%Zuweisungen
+Lm = parSys.Lm;
+Rm = parSys.Rm;
+cr1 = parSys.cr1;
+cr3 = parSys.cr3;
+rm = parSys.rm;
+dm = parSys.dm;
+rl = parSys.rl;
+dl = parSys.dl;
+Il = parSys.Il;
+Im = parSys.Im;
+p = parSys.p;
+Phi = parSys.Phi;
