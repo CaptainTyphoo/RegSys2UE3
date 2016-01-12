616bc49549f091cd2f778e49e07f2016358437f4
Andreas <abrewner@users.noreply.github.com>
2016-01-12 16:33:42 +0100

    slx Files aufgeräumt, Fehler in Fehlerdynamik mit C und D Matrix ausgebessert

--- Riementrieb/Fehlerdynamik.m
+++ Riementrieb/Fehlerdynamik.m
@@ -1,37 +1,27 @@
-
 parameter;
 Ta = parSys.TsID;
 
+% linearisiertes Fehlersystem
 Ae = [-dm / I_m 0.3e1 / 0.2e1 * Phi * p / I_m 0 0.2e1 * cr1 * rm / I_m 0; -0.2e1 / 0.3e1 * Phi * p / Lm -0.2e1 / 0.3e1 * Rm / Lm 0 0 0; 0 0 -0.2e1 / 0.3e1 * Rm / Lm 0 0; -rm 0 0 0 rl; 0 0 0 -0.2e1 * cr1 * rl / I_l -0.1e1 / I_l * dl;];
-
 b = 2/(3*Lm)*[0 0;1 0;0 1;0 0;0 0];
-
-c = eye(5);
-c(2,2) = 0;
-c(4,4) = 0;
-c(5,5) = 0;
-
-d = zeros(5,2);
+c = [1,0,0,0,0;0,0,1,0,0];
+d = zeros(2,2);
 
 sys_c = ss(Ae,b,c,d);
 sys_d = c2d(sys_c,Ta);
-
 sys_tustin = d2c(sys_d,'tustin');
 Gq = tf(sys_tustin);
 
-% Frequenzkennlinienverfahren
+% Frequenzkennlinienverfahren f?r Abtastsysteme
 
-
-%Übertragungsfktn von uq zu wm
+% ?bertragungsfktn von uq zu wm
 G_uqwm = Gq(1,1);
-%Übertragungsfunktion von ud zu id
-G_udid = Gq(3,2);
+% ?bertragungsfunktion von ud zu id
+G_udid = Gq(2,2);
 
-%erster PI Regler
-omega_0 = 2/Ta;
-%omega_c = 0.2*omega_0; %muss kleiner als 0.2*omega_0 sein
-%tr = 1.2/omega_c;
+% PI Regler
 
+% tr und ue direkt vorgeben
 tr1 = 10e-3;
 ue1 = 5;
 [Rz1, R1] = PI_Entwurf(G_udid,tr1,ue1,Ta);
@@ -40,27 +30,17 @@ tr2 = 10e-3;
 ue2 = 5;
 [Rz2, R2] = PI_Entwurf(G_uqwm,tr2,ue2,Ta);
 
-Gz = tf(sys_d);
-
-%geht nicht!
-%Tz2 = Rz2*Gz(1,1)/(1+Rz2*Gz(1,1));
-%Tz1 = Rz1*Gz(3,2)/(1+Rz1*Gz(3,2));
-
-
-
-T1 = R1*G_udid/(1+R1*G_udid);
-
-%die beiden Funktionen liefern (zum Glück) das gleiche :)
-T2 = minreal((R2*G_uqwm)/(1+R2*G_uqwm));
-%T2 = feedback(R2*G_uqwm,1);
-
-
-Tz1 = c2d(T1,Ta,'tustin');
-Tz2 = c2d(T2,Ta,'tustin');
-%Tz2 = feedback(Rz2*Gz(1,1),1);
- 
-
-%step(feedback(R2*G_uqwm,1));
-
 R1d = ss(Rz1);
 R2d = ss(Rz2);
+
+% Kontrolle
+Gz = tf(sys_d);
+T1 = feedback(Rz2*Gz(1,1),1);
+T2 = feedback(Rz1*Gz(2,2),1);
+
+p1=pole(T1);
+p2=pole(T2);
+
+% Pole beider geschlossenen Kreise innerhalb des Einheitskreises, 
+% aber nicht phasenminimal
+
--- Riementrieb/PI_Entwurf.m
+++ Riementrieb/PI_Entwurf.m
@@ -1,23 +1,20 @@
 function [ Rz, R ] = PI_Entwurf( G, tr, ue, Ta )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
 
 phi = 70 - ue;
 omega_c = 1.2/tr;
 
 s = tf('s');
 
-L1 = 1/s*G;
-L1_c = evalfr(L1,1i*omega_c);
-%absL1 = abs(L1_c);
+L1 = 1/s*G; % alle bekannten Terme des offenen Kreises
+L1_c = evalfr(L1,1i*omega_c); % ausgewertet an omega_c
 argL1 = angle(L1_c)*180/pi;
 if (argL1 > 180)
     argL1 = argL1 - 360;
 end
 
+%Phasenkorrektur
 argsoll = -180+phi;
 delta_arg = argsoll - argL1;
-
 Ti = tan(delta_arg*pi/180)/omega_c;
 
 L2 = L1*(1+Ti*s);
